ae2b73a55d6ecb5057c5023ab87946c28b5b9747
rsaengkrajai <rachanee.saengkrajai@agoda.com>
2017-02-13 23:18:26 +0700

    Complete it

--- scripts/app.js
+++ scripts/app.js
@@ -1,106 +1,77 @@
-var Cropper = (function() {
+var resultCanvas = document.getElementById('resultCanvas');
+var resultCtx = resultCanvas.getContext('2d');
+var img = new Image();  
 
-  var $size = 1200;
+$(function() {  
+  $('#upload').on('change', function() {
+    showUploadedImage(this);
+  });
+});
 
-  var $uploadCrop;
-  var $uploadedImageOrientation = 0;
-
-  var $profileShape = 'square';
-
-  function readFile(input) {
-		if (input.files && input.files[0]) {
-      $('.upload-msg').text('Reading Image');
-
+function showUploadedImage(input){ 
+  if (input.files && input.files[0]) {
       var reader = new FileReader();
-      reader.onload = function (e) {
-        $('.upload-msg').css('display', 'none');
-        $('.upload-result').css('display', 'inline-block');
-        $('.advanced-options').css('display', 'block');
-			  $('.cropper-area').addClass('ready');
 
-      	$uploadCrop.croppie('bind', {
-      		url: e.target.result,
-          zoom: 0
-      	}).then(function() {
-          console.log('jQuery bind complete');
-        });
+      reader.onload = function (e) {  
+          img.crossOrigin ='anonymous'; 
+          img.onload = function() {                             
+                            $('#outputImage').css('width',img.width);   
+                            $('#outputImage').css('height',img.height);          
+                            $('#outputImage').css('background-image', 'url(' + img.src + ')');
+                        };
+         img.src = e.target.result; 
       }
       reader.readAsDataURL(input.files[0]);
-    } else {
+  }   
+}
 
-    }
-	}
+function showTextOutput(){
+    var txt = $('#inputText').val();
+    $('#outputText').html(txt.replace(/\n\r?/g, '<br/>'));
+}
 
-  function showResult() {
-    $uploadCrop.croppie('result', {
-      type: 'canvas',
-      size: { width: $size, height: $size },
-      format: 'png'
-    }).then(function (resp) {
-      popupResult({
-        src: resp
-      });
-    });
-  }
+function buildResultAndDownload(){
+    resultCtx.clearRect(0, 0, resultCanvas.width, resultCanvas.height);
+      
+    $(resultCanvas).attr('width',img.width);   
+    $(resultCanvas).attr('height',img.height);          
+    resultCtx.drawImage(img, 0, 0);
+     
+    var divStyle = [
+                    'width:' + img.width +'px',
+                    'height:' + img.height +'px',
+                    'display:flex',
+                    'align-items:center',
+                    'justify-content: center',                    
+                    'text-align: center',
+                    'color: white',
+                    'text-shadow: 1px 0px gray',
+                    'font-weight: 300',
+                    'font-size: 1em',
+                     'font-family: Arial, Helvetica, sans-serif'
+                  ];  
 
-  function popupResult(result) {
-    $('.right-pane .processing-area').css('display', 'block');
-    $('.right-pane #result-wrapper').css('display', 'none');
+    var svgData = '<svg xmlns="http://www.w3.org/2000/svg">' +
+           '<foreignObject width="100%" height="100%">' +
+           '<div xmlns="http://www.w3.org/1999/xhtml" style="' + divStyle.join(';') + '">' +           
+            $('#outputText').html().replace(/<div>/g,'<br/>').replace(/<\/div>/g,'').replace(/<br>/g,'<br/>') +            
+           '</div>' +
+           '</foreignObject>' +
+           '</svg>';
 
-    var cnv = document.getElementById("result-canvas");
-    var ctx = cnv.getContext("2d");
+    var imgTxt = new Image();   
+    imgTxt.crossOrigin ='anonymous';
+    imgTxt.onload = function() {                
+                    resultCtx.drawImage(imgTxt, 0, 0);
+                    var link = document.createElement('a');
+                    link.download = "yourQuote.png";
+                    link.href = resultCanvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+                    link.click();                
+                };
+    imgTxt.src = buildSvgImageUrl(svgData);   
+}
 
-    var image = new Image();
-    image.src = result.src;
-    image.onload = function() {
-      var imageOverlay = new Image();
-      if ($profileShape == 'square')
-        imageOverlay.src = '/img/badge_left_' + $size + '.png';
-      else
-        imageOverlay.src = '/img/badge_left_circle_' + $size + '.png';
-      imageOverlay.onload = function() {
-        ctx.fillStyle = 'white';
-        ctx.fillRect(0, 0, $size, $size);
-        ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, image.width, image.height);
-        ctx.drawImage(imageOverlay, 0, 0, imageOverlay.width, imageOverlay.height, 0, 0, imageOverlay.width, imageOverlay.height);
-        var jpegUrl = cnv.toDataURL("image/jpeg");
-        $('#result-wrapper .image-area').html('<img src="' + jpegUrl + '" download="profile.jpg">');
-        $('#result-wrapper').css('display', 'block');
-        $('.right-pane .processing-area').css('display', 'none');
-      };
-    };
-  }
-
-  $uploadCrop = $('.cropper-area').croppie({
-      enableExif: true,
-      viewport: {
-          width: 280,
-          height: 280,
-          type: 'square'
-      },
-      boundary: {
-          width: 300,
-          height: 300
-      }
-  });
-  $('#upload').on('change', function () {
-    readFile(this);
-  });
-  $('.upload-result').on('click', function () { showResult(); });
-
-  $('input[type=radio][name=profile-shape]').change(function() {
-    $profileShape = this.value;
-    if ($profileShape == 'circle') {
-      $('#badge-overlay-left').css('display', 'none');
-      $('#circle-display-frame').css('display', 'inline-block');
-      $('#badge-overlay-left-circle').css('display', 'inline-block');
-    } else {
-      $('#badge-overlay-left').css('display', 'inline-block');
-      $('#circle-display-frame').css('display', 'none');
-      $('#badge-overlay-left-circle').css('display', 'none');
-    }
-  });
-
-  $profileShape = $('input[type=radio][name=profile-shape]').val();
-
-})();
+function buildSvgImageUrl(svgData) {
+    var b64 = window.btoa(unescape(encodeURIComponent(svgData)));
+    return 'data:image/svg+xml;base64,' + b64;
+}
